89f1f9d4a51e14df1893b624e6265d0dfcffaeb7
Ilyas Zalyalov <zalyalovir@gmail.com>
2021-05-21 13:29:22 +0300

    перевод на Фрагмент

--- app/src/main/java/com/example/myastrocode5/MainActivity.java
+++ app/src/main/java/com/example/myastrocode5/MainActivity.java
@@ -1,36 +1,41 @@
 package com.example.myastrocode5;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.myastrocode5.fragment.ExpandableListMain2;
 
 public class MainActivity extends AppCompatActivity {
 
-//    private ExpandableListMain forecast;
-//
-//    private FragmentManager manager;
-//    private FragmentTransaction transaction;
+    private ExpandableListMain2 forecast;
+
+    private FragmentManager manager;
+    private FragmentTransaction transaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//
-//        manager = getSupportFragmentManager();
-//        forecast = new ExpandableListMain();
+
+        manager = getSupportFragmentManager();
+        forecast = new ExpandableListMain2();
 
     }
 
-//    public void onClickExpandableListMain(View view) {
-//        transaction = manager.beginTransaction();
-//
-//        switch (view.getId()) {
-//            case R.id.button1:
-//                transaction.add(R.id.conteiner, forecast);
-//                break;
-//
-//        }
-//        transaction.commit();
-//    }
+    public void onClickExpandableListMain(View view) {
+        transaction = manager.beginTransaction();
+
+        switch (view.getId()) {
+            case R.id.button1:
+                transaction.add(R.id.conteiner, forecast);
+                break;
+
+        }
+        transaction.commit();
+    }
 
 }
